f26f2cd96ab63c5d30cde9525d8cfb71a51ec19e
Anthony Molinaro <anthonym@alumni.caltech.edu>
2011-03-21 21:52:35 +0000

    add trace messages

--- src/m_hash.c
+++ src/m_hash.c
@@ -46,26 +46,28 @@ m_hash_table_create(void)
   struct m_hash_table *hash_table = NULL;
 
   hash_table = (struct m_hash_table *) 
-               m_try_malloc0(sizeof(struct m_hash_table)); 
+    m_try_malloc0(sizeof(struct m_hash_table)); 
 
   if( hash_table != NULL )
-  {
-    hash_table->size = DEFAULT_SIZE;
-    hash_table->num = 0;
-    hash_table->nodes = 
-      (void **) m_try_malloc0( sizeof(void *) * hash_table->size );
-    if( hash_table->nodes != NULL )
     {
-      for( i=0; i < hash_table->size; ++i )
-      {
-        hash_table->nodes[i] = NULL;
-      }
-    } else {
-      /* we couldn't allocate the bins, free and return NULL */
-      m_free(hash_table);
-      return NULL;
+      hash_table->size = DEFAULT_SIZE;
+      hash_table->num = 0;
+      hash_table->nodes = 
+        (void **) m_try_malloc0( sizeof(void *) * hash_table->size );
+      if( hash_table->nodes != NULL )
+        {
+          for( i=0; i < hash_table->size; ++i )
+            {
+              hash_table->nodes[i] = NULL;
+            }
+        }
+      else
+        {
+          /* we couldn't allocate the bins, free and return NULL */
+          m_free(hash_table);
+          return NULL;
+        }
     }
-  }
 
   return hash_table;
 }
@@ -91,11 +93,13 @@ m_hash_table_get(struct m_hash_table *hash_table, const char *key)
   node = m_hash_table_get_node(hash_table, key);
 
   if( node == NULL )
-  {
-    return NULL;
-  } else {
-    return node->value;
-  }
+    {
+      return NULL;
+    }
+  else
+    {
+      return node->value;
+    }
 }
 
 /* sets a value, creating or overwriting if necessary */
@@ -108,50 +112,59 @@ m_hash_table_set(struct m_hash_table *hash_table, char *key, void *value)
   struct m_hash_node *tmp = NULL;
 
   if( hash_table != NULL && key != NULL && value != NULL )
-  {
-    /* see if the node exists */
-    tmp = m_hash_table_get_node(hash_table, key);
-
-    if( tmp != NULL ) /* we found an existing object at this key */
     {
-      /* free the object and replace it with the new one */
-      m_hash_free(tmp->key);
-      m_hash_free(tmp->value);
-      tmp->key = key;
-      tmp->value = value;
-      return 0;
-    }
+      /* see if the node exists */
+      tmp = m_hash_table_get_node(hash_table, key);
 
-    /* allocate the new node */
-    node = (struct m_hash_node *) m_try_malloc0(sizeof(struct m_hash_node)); 
-    if( node != NULL )
-    {
-      node->key = key;
-      node->value = value;
-      node->next = NULL;
-
-      index = m_hash_function(key) % hash_table->size;
-      if( hash_table->nodes[index] == NULL )
-      {
-        /* this bucket is empty so just add it */
-        hash_table->nodes[index] = (void *) node;
-      } else {
-        /* get to the end of the bucket and append it */
-        iterator = (struct m_hash_node *) hash_table->nodes[index];
-        while( iterator->next != NULL ) 
+      if( tmp != NULL ) /* we found an existing object at this key */
         {
-          iterator = iterator->next;
+          /* free the object and replace it with the new one */
+          m_hash_free(tmp->key);
+          m_hash_free(tmp->value);
+          tmp->key = key;
+          tmp->value = value;
+          return 0;
         }
-        iterator->next = node;
-      }
 
-      /* increment the count */
-      hash_table->num++;
-    } else {
-      /* malloc failed */
-      return -3;
+      /* allocate the new node */
+      node = (struct m_hash_node *) m_try_malloc0(sizeof(struct m_hash_node)); 
+      if( node != NULL )
+        {
+          node->key = key;
+          node->value = value;
+          node->next = NULL;
+
+          index = m_hash_function(key) % hash_table->size;
+          if( hash_table->nodes[index] == NULL )
+            {
+              /* this bucket is empty so just add it */
+              hash_table->nodes[index] = (void *) node;
+            }
+          else
+            {
+              /* get to the end of the bucket and append it */
+              iterator = (struct m_hash_node *) hash_table->nodes[index];
+              while( iterator->next != NULL ) 
+                {
+                  iterator = iterator->next;
+                }
+              iterator->next = node;
+            }
+
+          /* increment the count */
+          hash_table->num++;
+        }
+      else
+        {
+          /* malloc failed */
+          return -3;
+        }
     }
-  }
+  else
+    {
+      /* null hash table */
+      return -4;
+    }
 
   return 0;
 }
@@ -165,47 +178,49 @@ m_hash_table_remove(struct m_hash_table *hash_table, const char *key)
   struct m_hash_node *prev = NULL;
 
   if(hash_table != NULL && key != NULL)
-  {
-    /* look for the element to remove */
-    index = m_hash_function(key) % hash_table->size;
-    head = (struct m_hash_node *) hash_table->nodes[index];
-    if( head == NULL )
     {
-      return;
-    }
+      /* look for the element to remove */
+      index = m_hash_function(key) % hash_table->size;
+      head = (struct m_hash_node *) hash_table->nodes[index];
+      if( head == NULL )
+        {
+          return;
+        }
 
-    iterator = head;
-    prev = head;
-    while( iterator != NULL && strcmp(iterator->key, key) != 0 )
-    {
-      prev = iterator;
-      iterator = iterator->next;
-    }
+      iterator = head;
+      prev = head;
+      while( iterator != NULL && strcmp(iterator->key, key) != 0 )
+        {
+          prev = iterator;
+          iterator = iterator->next;
+        }
 
-    /* if we don't find anything just stop */
-    if( iterator == NULL )
-    {
-      return;
-    }
+      /* if we don't find anything just stop */
+      if( iterator == NULL )
+        {
+          return;
+        }
 
-    if( iterator == head )
-    {
-      /* if first element in the list, set the first element to the next */
-      hash_table->nodes[index] = (void *) iterator->next;
-    } else {
-      /* otherwise, remove ourselves by pointing the previous entry at 
-         the next */
-      prev->next = iterator->next;
-    }
+      if( iterator == head )
+        {
+          /* if first element in the list, set the first element to the next */
+          hash_table->nodes[index] = (void *) iterator->next;
+        }
+      else
+        {
+          /* otherwise, remove ourselves by pointing the previous entry at 
+             the next */
+          prev->next = iterator->next;
+        }
 
-    /* remove this entry */
-    iterator->next = NULL;
-    m_hash_free(iterator->key);
-    m_hash_free(iterator->value);
-    m_hash_free(iterator);
-    
-    hash_table->num--;
-  }
+      /* remove this entry */
+      iterator->next = NULL;
+      m_hash_free(iterator->key);
+      m_hash_free(iterator->value);
+      m_hash_free(iterator);
+
+      hash_table->num--;
+    }
 }
 
 void
@@ -216,32 +231,32 @@ m_hash_table_remove_all(struct m_hash_table *hash_table)
   struct m_hash_node *marked = NULL;
 
   if( hash_table != NULL )
-  {
-    if( hash_table->nodes != NULL )
     {
-      /* go through all buckets */
-      for( i=0; i<hash_table->size; ++i)
-      {
-        /* go throuh all the entries */
-        iterator = hash_table->nodes[i];
-        while( iterator != NULL )
+      if( hash_table->nodes != NULL )
         {
-          marked = iterator;
-          iterator = iterator->next;
+          /* go through all buckets */
+          for( i=0; i<hash_table->size; ++i)
+            {
+              /* go throuh all the entries */
+              iterator = hash_table->nodes[i];
+              while( iterator != NULL )
+                {
+                  marked = iterator;
+                  iterator = iterator->next;
 
-          m_hash_free(marked->key);
-          m_hash_free(marked->value);
-          m_hash_free(marked);
+                  m_hash_free(marked->key);
+                  m_hash_free(marked->value);
+                  m_hash_free(marked);
+                }
+
+              /* wipe out the reference */
+              hash_table->nodes[i] = NULL;
+            }
+
+          /* reset the num */
+          hash_table->num = 0;
         }
-
-        /* wipe out the reference */
-        hash_table->nodes[i] = NULL;
-      }
-
-      /* reset the num */
-      hash_table->num = 0;
     }
-  }
 }
 
 /* return an immutable list of keys */
@@ -254,25 +269,25 @@ m_hash_table_keys(struct m_hash_table *hash_table)
   struct m_hash_node *iterator = NULL;
 
   if( hash_table != NULL )
-  {
-    keys = (char **) m_try_malloc0( sizeof(char *) * (hash_table->num + 1) ); 
-
-    if( keys != NULL )
     {
-      for( i=0; i<hash_table->size; ++i )
-      {
-        iterator = hash_table->nodes[i];
-        while( iterator != NULL )
-        {
-          keys[j++] = (char *) iterator->key;
-          iterator = iterator->next;
-        } 
-      }
-      keys[j] = NULL;
+      keys = (char **) m_try_malloc0( sizeof(char *) * (hash_table->num + 1) ); 
 
-      return (const char **) keys;
-    }
-  } /* if( hash_table != NULL ) */
+      if( keys != NULL )
+        {
+          for( i=0; i<hash_table->size; ++i )
+            {
+              iterator = hash_table->nodes[i];
+              while( iterator != NULL )
+                {
+                  keys[j++] = (char *) iterator->key;
+                  iterator = iterator->next;
+                } 
+            }
+          keys[j] = NULL;
+
+          return (const char **) keys;
+        }
+    } /* if( hash_table != NULL ) */
 
   return NULL;
 }
@@ -288,14 +303,14 @@ m_hash_function(const char *key)
   int i = 0;
 
   if( key == NULL )
-  {
-    return 0;
-  }
+    {
+      return 0;
+    }
 
   for( i=0; key[i] != '\0'; ++i )
-  {
-    sum += ((int) key[i]) * ((int) key[i]);
-  }
+    {
+      sum += ((int) key[i]) * ((int) key[i]);
+    }
 
   return sum;
 }
@@ -318,27 +333,27 @@ m_hash_table_get_node(struct m_hash_table *hash_table, const char *key)
   struct m_hash_node *iterator = NULL;
 
   if( hash_table != NULL && key != NULL )
-  {
-    index = m_hash_function(key) % hash_table->size;
-    bucket = hash_table->nodes[index];
-    if( bucket == NULL )
     {
-      return NULL;
-    }
+      index = m_hash_function(key) % hash_table->size;
+      bucket = hash_table->nodes[index];
+      if( bucket == NULL )
+        {
+          return NULL;
+        }
 
-    iterator = bucket;
-    while( iterator != NULL && strcmp(iterator->key, key) != 0 )
-    {
-      iterator = iterator->next;
-    }
+      iterator = bucket;
+      while( iterator != NULL && strcmp(iterator->key, key) != 0 )
+        {
+          iterator = iterator->next;
+        }
 
-    if( iterator == NULL )
-    {
-      return NULL;
-    }
+      if( iterator == NULL )
+        {
+          return NULL;
+        }
 
-    return iterator;
-  } else {
-    return NULL;
-  }
+      return iterator;
+    } else {
+        return NULL;
+    }
 }
